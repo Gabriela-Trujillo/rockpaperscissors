6103e21c6dcacabac85eccfa4889026bfe35a62c
Gabriela Trujillo <gaby47081@gmail.com>
2018-10-18 00:52:51 +0000

    created a click function and a variable in jquery

--- js/script.js
+++ js/script.js
@@ -9,3 +9,9 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+$("#shoot").click(function(){
+    var computerchoice = "rock";
+    $("#userChoice").text($("#input").val())
+    $("#computerChoice").text(computerchoice)
+});
+
